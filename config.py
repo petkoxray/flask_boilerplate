7abdeb312e0d16eb0fa3281835dfaab00563c09d
petkoxray <petkoxray@gmail.com>
2018-11-10 13:16:14 +0200

    added .env file for configuration

--- config.py
+++ config.py
@@ -1,15 +1,22 @@
 import os
+from dotenv import load_dotenv
 
 basedir = os.path.abspath(os.path.dirname(__file__))
+load_dotenv(os.path.join('.env'))
 
 
 class Config:
     # App configurations
     SECRET_KEY = os.environ.get('SECRET_KEY') or "very-secret-key"
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') \
-                              or 'sqlite:///' + os.path.join(basedir, 'site.db')
-    MAIL_SERVER = 'smtp.googlemail.com'
-    MAIL_PORT = 587
-    MAIL_USE_TLS = True
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+    #Mail Server configurations
+    MAIL_SERVER = os.environ.get('EMAIL_SERVER')
+    MAIL_PORT = os.environ.get('EMAIL_PORT')
+    MAIL_USE_TLS = os.environ.get('EMAIL_TLS')
     MAIL_USERNAME = os.environ.get('EMAIL_USER')
     MAIL_PASSWORD = os.environ.get('EMAIL_PASS')
+
+    # Debug Toolbar
+    DEBUG_TB_INTERCEPT_REDIRECTS = False
